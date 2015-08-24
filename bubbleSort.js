2f2bdab73e726fb01f8cac02b78e9f70b4ac0721
benhassara <bhassara@gmail.com>
2015-08-24 10:21:58 -0600

    finish bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,20 @@
 function bubbleSort(arr){
+  var wrk = arr.slice();
+  var swapped = true;
 
+  while(swapped) {
+    swapped = false;
+    for (var i = 0; i < wrk.length-1; i++) {
+      if (wrk[i] > wrk[i+1]) {
+        var tmp = wrk[i];
+        wrk[i] = wrk[i+1];
+        wrk[i+1] = tmp;
+        swapped = true;
+      }
+    }
+  }
+
+  return wrk;
 }
 
 module.exports = bubbleSort;
